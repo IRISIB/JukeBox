9e62849dafac7b4d51a392aeb271f517ca8404bf
Max <marlier.maxime@gmail.com>
2015-02-22 16:02:15 +0100

    import real user playlist when view is call

--- JukeBox/player/views.py
+++ JukeBox/player/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from player.models import import_playlists
 from player.models import Playlist
 
 def index(request):
+	import_playlists()
 	playlists = Playlist.objects.all()
 	return render(request, 'player/index.html', {'playlists' : playlists})
 
