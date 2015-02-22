fc91eda70e2915b2086d7359c8d4afee70a3a934
Max <marlier.maxime@gmail.com>
2015-02-22 16:00:18 +0100

    [add] every models to admin modification

--- JukeBox/player/admin.py
+++ JukeBox/player/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from player.models import Playlist
+from player.models import Playlist, Track, Artist, PlaylistEntry
 
 admin.site.register(Playlist)
+admin.site.register(Track)
+admin.site.register(Artist)
+admin.site.register(PlaylistEntry)
 
 # Register your models here.
 
