3f2323ff3f45a01c88f0b69d7ea5d70b570b9a53
Makcime <marlier.maxime@gmail.com>
2015-02-25 16:28:30 +0100

    [enh] redirect the default url to the player application

--- JukeBox/JukeBox/urls.py
+++ JukeBox/JukeBox/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.contrib import admin 
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'JukeBox.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', RedirectView.as_view(url='/player/')),
     url(r'^player/', include('player.urls')),
     url(r'^admin/', include(admin.site.urls)),    
 )
