35baf60d4fcd60b3c7cd4ff5d6900e8672556fbc
Makcime <marlier.maxime@gmail.com>
2015-03-29 15:38:38 +0200

    [add] views for json formating [add] view to test ajax [ref] autoformat with anaconda

--- player/views.py
+++ player/views.py
@@ -1,93 +1,134 @@
 from django.shortcuts import get_object_or_404, render
 import json
+import inspect
+from django.core import serializers
+
 
 # Create your views here.
 
 from django.http import HttpResponse
-from manager.models import Playlist, PlaylistEntry, import_playlists, Track
+from manager.models import Playlist, PlaylistEntry, import_playlists, Track, Artist
+
 
 def index(request):
-	playlists = Playlist.objects.all()
-	return render(request, 'player/index.html', {'playlists' : playlists})
+    playlists = Playlist.objects.all()
+    return render(request, 'player/index.html', {'playlists': playlists})
+
 
 def update(request):
-	import_playlists()
-	playlists = Playlist.objects.all()
-	return render(request, 'player/index.html', {'playlists' : playlists})
+    import_playlists()
+    playlists = Playlist.objects.all()
+    return render(request, 'player/index.html', {'playlists': playlists})
+
 
 def select(request):
-	playlists = Playlist.objects.all()
-	return render(request, 'player/index.html', {'playlists' : playlists})
+    playlists = Playlist.objects.all()
+    return render(request, 'player/index.html', {'playlists': playlists})
+
 
 def playing(request):
-	'''
-	Premiere connexion au lecteur
-	'''
-	playlist = get_object_or_404(Playlist, DeezerId=int(request.POST['choice']))
-	track_list = PlaylistEntry.objects.all().filter(PlaylistId=playlist.id)
-	
-	track_player = True
-	player_type = 'tracks'
+    '''
+    Premiere connexion au lecteur
+    '''
+    playlist = get_object_or_404(
+        Playlist, DeezerId=int(request.POST['choice']))
+    track_list = PlaylistEntry.objects.all().filter(PlaylistId=playlist.id)
 
-	request.session['playlist_id'] = playlist.DeezerId
-	request.session['indice'] = 0
+    track_player = True
+    player_type = 'tracks'
 
-	current_track = get_object_or_404(Track, DeezerId=track_list[0].TrackId.DeezerId)
-	request.session['current_track_id'] = current_track.DeezerId
+    request.session['playlist_id'] = playlist.DeezerId
+    request.session['indice'] = 0
 
-	context = {	'playlist' : playlist, 
-				'track_list'  : track_list, 
-				'type' : player_type , 
-				'track_player' : track_player,
-				'current_track' : current_track}
-	
-	return render(request, 'player/playing.html', context)
+    current_track = get_object_or_404(
+        Track, DeezerId=track_list[0].TrackId.DeezerId)
+    request.session['current_track_id'] = current_track.DeezerId
+
+    context = {	'playlist': playlist,
+                'track_list': track_list,
+                'type': player_type,
+                'track_player': track_player,
+                'current_track': current_track}
+
+    return render(request, 'player/playing.html', context)
+
 
 def track_playing(request):
-	'''
-	Automatic track playing
-	'''
-	playlist_id = request.session.get('playlist_id')
-	playlist = get_object_or_404(Playlist, DeezerId=int(playlist_id))
+    '''
+    Automatic track playing
+    '''
+    playlist_id = request.session.get('playlist_id')
+    playlist = get_object_or_404(Playlist, DeezerId=int(playlist_id))
 
-	track_list = PlaylistEntry.objects.all().filter(PlaylistId=playlist.id)
-	
-	current_track = get_object_or_404(Track, DeezerId=track_list[request.session.get('indice') + 1].TrackId.DeezerId)
-	request.session['indice'] = request.session.get('indice') + 1
-	request.session['current_track_id'] = current_track.DeezerId
-	# current_track = get_object_or_404(Track, DeezerId=request.session.get('current_track_id'))
+    track_list = PlaylistEntry.objects.all().filter(PlaylistId=playlist.id)
 
-	# current_track = get_object_or_404(Track, DeezerId=request.session.get('current_track_id'))
-	# current_track = Track.objects.all().filter(DeezerId=request.session.get('current_track_id'))
+    current_track = get_object_or_404(
+        Track, DeezerId=track_list[request.session.get('indice') + 1].TrackId.DeezerId)
 
-	track_player = True
-	player_type = 'tracks'
+    track_list = PlaylistEntry.objects.all().filter(PlaylistId=playlist.id)
 
-	context = {	'playlist' : playlist, 
-				'track_list'  : track_list, 
-				'type' : player_type , 
-				'track_player' : track_player,
-				'current_track' : current_track}
-	
-	return render(request, 'player/playing.html', context)
+    current_track = get_object_or_404(
+        Track, DeezerId=track_list[request.session.get('indice') + 1].TrackId.DeezerId)
+    request.session['indice'] = request.session.get('indice') + 1
+    request.session['current_track_id'] = current_track.DeezerId
+    # current_track = get_object_or_404(Track, DeezerId=request.session.get('current_track_id'))
+
+    # current_track = get_object_or_404(Track, DeezerId=request.session.get('current_track_id'))
+    # current_track = Track.objects.all().filter(DeezerId=request.session.get('current_track_id'))
+
+    track_player = True
+    player_type = 'tracks'
+
+    context = {	'playlist': playlist,
+                'track_list': track_list,
+                'type': player_type,
+                'track_player': track_player,
+                'current_track': current_track}
+
+    return render(request, 'player/playing.html', context)
 
 
 def test_ajax(request):
-	print 'salut'
-	results = {'Duration': 'NONE'}
-	if request.method == u'GET':
-	    GET = request.GET
-	    if GET.has_key(u'pk') and GET.has_key(u'time'):
-	        pk = int(GET[u'pk'])
-	        time = GET[u'time']
-	    	print pk, time
-	        track = Track.objects.get(pk=pk)
-	        results
-	        if time == u"min":
-	            duration = track.getMinutes()
-	        elif time == u"usec":
-	            duration = track.getuSec()
-	        results = {'duration': duration,
-	        			'title' : track.title}
-	myjson = json.dumps(results)
-	return HttpResponse(myjson, content_type='application/json')
+    print 'salut'
+    results = {'Duration': 'NONE'}
+    if request.method == u'GET':
+        GET = request.GET
+        if GET.has_key(u'pk') and GET.has_key(u'time'):
+            pk = int(GET[u'pk'])
+            time = GET[u'time']
+            print pk, time
+            track = Track.objects.get(pk=pk)
+            results
+            if time == u"min":
+                duration = track.getMinutes()
+            elif time == u"usec":
+                duration = track.getuSec()
+            results = {'duration': duration,
+                       'title': track.title}
+    myjson = json.dumps(results)
+    return HttpResponse(myjson, content_type='application/json')
+
+
+def test_design(request):
+    return render(request, 'player/neon_player.html', {})
+
+
+def current_track_list(request):
+    playlist_id = request.session.get('playlist_id')
+    playlist = get_object_or_404(Playlist, DeezerId=int(playlist_id))
+
+    track_list = PlaylistEntry.objects.all().filter(PlaylistId=playlist.id)
+    dplaylist = playlist.to_dict()
+    # current_track = Track.objects.get(DeezerId=track_list[request.session.get('indice') + 1].TrackId.DeezerId)
+
+    ldtracks = []
+
+    for entry in track_list:
+        ldtracks.append(entry.TrackId.to_dict())
+
+    data = {"playlist": dplaylist,
+            "tracks": ldtracks}
+
+    jsData = json.dumps(data, sort_keys=True, indent=2)
+
+    return HttpResponse(jsData, content_type='application/json')
