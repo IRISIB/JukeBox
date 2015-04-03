1223837f6e771b6425617d3fe62dd9bb7b773d2b
Lidouh <samia_lidouh@hotmail.com>
2015-04-03 11:39:43 +0200

    Ajout de la fonction limitedPlaylist dans server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -13,13 +13,17 @@ var playlist = "Aucune playlist n'a été mise en ligne";
 var playlistOnline = false;
 var votes = [];
 var current_trackId;
+var limit;
 
 var nsp_manager = io.of('/manager');
 nsp_manager.on('connection', function(socket){
   console.log('Manager connected');
   socket.emit('connected', 'Hi Manager!');
   socket.on('playlistOn', function (message) {
-  		playlist = message;
+  		limit = 5;
+  	if (JSON.parse(message).Tracks.length > limit){
+  		playlist = limitedPlaylist(message, limit);
+  	} else { playlist = message ; }
 		playlistOnline = true;
         console.log('Manager says : ' + JSON.parse(playlist).title);
 		nsp_player.emit('playlistOn', playlist);
@@ -45,6 +49,20 @@ nsp_manager.on('connection', function(socket){
 	
 });
 
+function limitedPlaylist(playlist, limit){
+	var newPlaylist = playlist;
+	var newPlaylistJSon = JSON.parse(newPlaylist);
+	for(var index in newPlaylistJSon.Tracks){
+			newPlaylistJSon.Tracks.splice(0, newPlaylistJSon.Tracks.length);
+		}
+		console.log(newPlaylistJSon);
+	for(var i = 0, l = limit; i < l; i++) {
+          	newPlaylistJSon.Tracks.push(JSON.parse(playlist).Tracks[i]);
+        }
+        console.log(newPlaylistJSon);
+     newPlaylist = JSON.stringify(newPlaylistJSon);
+    return newPlaylist;
+}
 
 function reset(){ 
 	if(playlistOnline){
