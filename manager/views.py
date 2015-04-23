ac95c884dcf3ed41fe218cd0cac19824501133a2
Makcime <marlier.maxime@gmail.com>
2015-04-23 17:23:07 +0200

    [add] view to create a niew session

--- manager/views.py
+++ manager/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from models import Playlist, PlaylistEntry, import_playlists, PlaylistSession
+
+
 # Create your views here.
+
+
 def index(request):
-	return HttpResponse("Hello, world. You're at the manager page.")
+    return HttpResponse("Hello, world. You're at the manager page.")
+
+
+def createSession(request):
+    new_session = PlaylistSession()
+    new_session.Session = '{""}'
+    new_session.save()
+    return HttpResponse("Hello, world. You're at the manager page.")
