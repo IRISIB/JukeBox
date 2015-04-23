ea2409b23968ba9111ec3700fde1aaa4b3c47f91
Makcime <marlier.maxime@gmail.com>
2015-04-23 17:21:36 +0200

    [test] link index to create session

--- manager/urls.py
+++ manager/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from manager import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.createSession, name='index'),
 )
