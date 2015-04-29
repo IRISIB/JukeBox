d4803af3b7281c9e44c7162383e5bc4816925856
Makcime <marlier.maxime@gmail.com>
2015-04-29 10:45:16 +0200

    [add] view to create a new playlist session

--- manager/views.py
+++ manager/views.py
@@ -12,7 +12,42 @@ def index(request):
 
 
 def createSession(request):
-    new_session = PlaylistSession()
-    new_session.Session = '{""}'
+    '''
+    Create a new playlist session (json format) from 
+    a playlist id in a post request and 
+    save it in th db
+    '''
+    if request.POST['choice']:
+	    playlist_id =  request.POST['choice']
+	    playlist = get_object_or_404(Playlist, DeezerId=int(playlist_id))
+
+	    dplaylist = playlist.to_dict()
+
+	    track_list = PlaylistEntry.objects.all().filter(PlaylistId=playlist.id)
+	    ldtracks = []
+
+	    for entry in track_list:
+	        ldtracks.append(entry.TrackId.to_dict())
+
+
+	    current_track = track_list[0]
+	    dcurrent_track = current_track.to_dict()
+
+	    data = {"playlist": dplaylist,
+	            "current_track": dcurrent_track,
+	            "tracks": ldtracks}
+
+	    jsData = json.dumps(data, sort_keys=True, indent=4)
+
+	    if request.GET['callback']:
+	        jspData = request.GET['callback']+'(' + jsData + ');'
+
+	    new_session = PlaylistSession()
+
+    else :
+        jspData = '{""}'
+
+	new_session.Session = jspData
     new_session.save()
-    return HttpResponse("Hello, world. You're at the manager page.")
+
+    return HttpResponse(jspData, content_type='application/json') 
