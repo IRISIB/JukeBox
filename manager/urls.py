0cb8949c95a5a6ceeaa5d0b48ff291dbfcd84a54
Makcime <marlier.maxime@gmail.com>
2015-04-29 10:45:47 +0200

    [add] link to new views

--- manager/urls.py
+++ manager/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url
 from manager import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.createSession, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'createSession/^$', views.createSession, name='newSession'),
+    url(r'getSession/^$', views.createSession, name='newSession'),
 )
