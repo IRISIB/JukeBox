c863daf31783c52ed1dd846cf0aaf2811571a366
Makcime <marlier.maxime@gmail.com>
2015-05-21 02:49:16 +0200

    [add] playlist session

--- manager/admin.py
+++ manager/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from manager.models import Playlist, Track, Artist, PlaylistEntry
+from manager.models import Playlist, Track, Artist, PlaylistEntry, PlaylistSession
 
 admin.site.register(Playlist)
 admin.site.register(Track)
 admin.site.register(Artist)
 admin.site.register(PlaylistEntry)
+admin.site.register(PlaylistSession)
 
 # Register your models here.
