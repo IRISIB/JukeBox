e7fdb0ac63ef2ef6ace7465d35125f5863747667
Makcime <marlier.maxime@gmail.com>
2015-05-21 02:05:43 +0200

    [add] url for session management

--- manager/urls.py
+++ manager/urls.py
@@ -7,4 +7,7 @@ urlpatterns = patterns('',
     url(r'^selection/', views.select, name='select'),
     url(r'^update/', views.update, name='update'),
     url(r'^playing/', views.playing, name='play'),
+    url(r'^createSession/', views.createSession, name='newSession'),
+    url(r'^getSession/', views.getSession, name='getSession'),
+    url(r'^next/', views.nextTrack, name='next'),
 )
